488d9c1e60a311de23cb4e93b6b9c7ae5b6d43ef
Phurich <pusapanich.phurich@gmail.com>
2024-01-04 23:05:50 +1100

    2 upstream independent dags

--- python_operator_sample.py
+++ python_operator_sample.py
@@ -14,9 +14,14 @@ def get_name(ti):
     ti.xcom_push(key='last_name', value='Koni')
 
 
-def greet(age, ti):
+def get_age(ti):
+    ti.xcom_push(key='age', value=29)
+
+
+def greet(ti):
     name = ti.xcom_pull(task_ids='get_name', key='first_name')
     last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
     print(f"Hello, name is {name} {last_name} and I am {age} years old")
 
 
@@ -30,13 +35,17 @@ with DAG(
 
     task1 = PythonOperator(
         task_id="greet",
-        python_callable=greet,
-        op_kwargs={'age': 29}
+        python_callable=greet
     )
 
     task2 = PythonOperator(
         task_id="get_name",
-        python_callable=get_name,
+        python_callable=get_name
     )
 
-    task2 >> task1
+    task3 = PythonOperator(
+        task_id="get_age",
+        python_callable=get_age
+    )
+
+    [task3, task2] >> task1

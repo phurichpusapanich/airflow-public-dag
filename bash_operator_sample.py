a24801254796e72cdc72d08a1d5d9a82670ac66b
Phurich <pusapanich.phurich@gmail.com>
2024-01-04 16:03:21 +1100

    Add downstream task 2

--- bash_operator_sample.py
+++ bash_operator_sample.py
@@ -9,7 +9,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id='bash_operator_sample',
+    dag_id='bash_operator_sample_v2',
     default_args=default_args,
     description='This is a sample version of bash operator',
     start_date=datetime(2024, 1, 4, 15),
@@ -18,5 +18,12 @@ with DAG(
 ) as dag:
     task1 = BashOperator(
         task_id='first_task',
-        bash_command="echo 'hello world!'"
+        bash_command="echo hello world, this is the first task"
     )
+
+    task2 = BashOperator(
+        task_id='second_task',
+        bash_command="echo Hey, I am task 2!"
+    )
+
+    task1.set_downstream(task2)

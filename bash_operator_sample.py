e22b9cb2fdcc393cc47af5999f989de4b4dd7802
Phurich <pusapanich.phurich@gmail.com>
2024-01-04 15:38:47 +1100

    Add bash operator  dag

---
+++ bash_operator_sample.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BaseOperator
+
+default_args = {
+    'owner': 'Niche',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG(
+    dag_id='bash_operator_sample',
+    default_args=default_args,
+    description='This is a sample version of bash operator',
+    start_date=datetime(2024, 1, 4, 15),
+    schedule_interval='@daily'
+
+) as dag:
+    task1 = BaseOperator(
+        task_id='first_task',
+        bash_command="echo 'hello world!'"
+    )

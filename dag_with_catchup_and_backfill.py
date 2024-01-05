0cb8784a0cd332a7f5eda516cb1395f8cddb900c
Phurich <pusapanich.phurich@gmail.com>
2024-01-05 17:28:22 +1100

    add backfill and catchup

---
+++ dag_with_catchup_and_backfill.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+import logging
+import pendulum
+logging.basicConfig(level=logging.DEBUG)
+
+default_args = {
+    'owner': 'Niche',
+}
+
+with DAG(
+    dag_id='backfill_and_catch_up',
+    default_args=default_args,
+    description='Sample of backfill and catchup',
+    start_date=pendulum.datetime(2024, 1, 4, tz="UTC"),
+    schedule_interval='@daily'
+
+) as dag:
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command='echo "Hello World Task 1" && sleep 1'
+    )
+
+    task1
